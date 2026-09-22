cdd3e1288e24a1cbef427c71fcc3f2932a90aa22
ayasadiq04 <aya04sadiq@gmail.com>
2026-09-22 13:03:37 +0000

    fix: add Alpine.js to Vite bundle to fix dropdown and notification bell

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,6 @@
+import Alpine from 'alpinejs';
+window.Alpine = Alpine;
+Alpine.start();
 
 document.addEventListener('click', (event) => {
     const toggle = event.target.closest('[data-password-toggle]');
